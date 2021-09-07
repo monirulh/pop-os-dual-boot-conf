1e0e308ac8a83c8f64a2efce023eb2c5d2e09bea
Monirul Hasan <monirul@pop-os.localdomain>
2021-09-07 23:03:24 +0600

    initial commit

---
+++ pop_os_win_dual_boot_set_up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+suod apt-get update && sudo apt-get upgrade -y
+sudo apt-get install grub-efi grub-customizer grub2-common -y
+
+sudo grub-install
+sudo cp /boot/grub/x86_64-efi/grub.efi /boot/efi/EFI/pop/grubx64.efi
+sudo cp ./grub.cfg /boot/efi/EFI/pop/
+sudo update-grub
